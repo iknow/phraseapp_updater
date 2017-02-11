a65e3ab58782ca48c769ca68e2071a6b07f488ce
Kevin Griffin <kev@bibo.com.ph>
2017-02-11 18:49:06 +0900

    More spec

--- spec/phraseapp_updater/differ_spec.rb
+++ spec/phraseapp_updater/differ_spec.rb
@@ -2,15 +2,21 @@ require 'spec_helper'
 require 'phraseapp_updater/differ'
 
 describe Differ do
-  subject { Differ.resolve!(original: original, primary: a, secondary: b) }
+  subject { Differ.resolve!(original: original, primary: @a, secondary: @b) }
 
-  context 'simple diffs' do
+  context 'empty base' do
     let(:original) { {} }
-    let(:a) { {"a" => 4} }
-    let(:b) { {"b" => 5} }
 
-    it 'resolves simple diffs' do
-      expect(subject).to eq({"a" => 4, "b" => 5})
+    context 'non-conflicting diffs' do
+
+      it 'resolves simple diffs' do
+        @a = {"a" => 4}
+        @b = {"b" => 5}
+        expect(subject).to eq({"a" => 4, "b" => 5})
+      end
+
+      it 'adds keys to an existing hash' do
+      end
     end
   end
 end
